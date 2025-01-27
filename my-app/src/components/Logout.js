fbe6c86650761c60b0808442dfcccbb5f49755a5
AyushO0 <ayushoficial04@gmail.com>
2025-01-27 14:48:32 +0530

    Update Logout.js

--- my-app/src/components/Logout.js
+++ my-app/src/components/Logout.js
@@ -1,18 +1,1 @@
-import React from "react";
-import { useAuth } from "../utils/authContext";
 
-const Logout = () => {
-  const { setUser } = useAuth();
-
-  const handleLogout = () => {
-    setUser(null);
-  };
-
-  return (
-    <button className="logout-button" onClick={handleLogout}>
-      Logout
-    </button>
-  );
-};
-
-export default Logout;
