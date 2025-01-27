ad04b3f4b234687cca3088cea6cb4dd6a70c87b2
harshh0609 <113016898+harshh0609@users.noreply.github.com>
2025-01-27 22:24:38 +0530

    Delete my-app directory

--- my-app/src/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://nkvictbugbscfagazmnw.supabase.co";
-const supabaseKey =
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rdmljdGJ1Z2JzY2ZhZ2F6bW53Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5NTIwOTksImV4cCI6MjA1MDUyODA5OX0.t1rZznUyhCQ-HJmJeuqP-ZGKOpqWx8rwkazKSzzFwmI";
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
